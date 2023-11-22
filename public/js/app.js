fbf5f5af1ae800783347558337d39ce41d707f2e
bogdanwoj <you@example.combogdanwojcicki31@gmail.com>
2023-11-22 15:19:39 +0200

    added layout for checkout payment

--- public/js/app.js
+++ public/js/app.js
@@ -22720,35 +22720,36 @@ var _hoisted_9 = {
 var _hoisted_10 = {
   "class": "plan-price"
 };
-var _hoisted_11 = {
+var _hoisted_11 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div><!--SHIPPING METHOD--><div class=\"panel panel-info\"><div class=\"panel-body\"><div class=\"form-group\"><div class=\"col-md-12\"><h4>Shipping Address</h4><br></div></div><div class=\"form-group\"><div class=\"col-md-12\"><strong>Country:</strong></div><div class=\"col-md-12\"><input type=\"text\" class=\"form-control\" name=\"country\" value=\"\"></div></div><div class=\"form-group\"><div class=\"col-md-6 col-xs-12\"><strong>First Name:</strong><input type=\"text\" name=\"first_name\" class=\"form-control\" value=\"\"></div><div class=\"span1\"></div><div class=\"col-md-6 col-xs-12\"><strong>Last Name:</strong><input type=\"text\" name=\"last_name\" class=\"form-control\" value=\"\"></div></div><div class=\"form-group\"><div class=\"col-md-12\"><strong>Address:</strong></div><div class=\"col-md-12\"><input type=\"text\" name=\"address\" class=\"form-control\" value=\"\"></div></div><div class=\"form-group\"><div class=\"col-md-12\"><strong>City:</strong></div><div class=\"col-md-12\"><input type=\"text\" name=\"city\" class=\"form-control\" value=\"\"></div></div><div class=\"form-group\"><div class=\"col-md-12\"><strong>State:</strong></div><div class=\"col-md-12\"><input type=\"text\" name=\"state\" class=\"form-control\" value=\"\"></div></div><div class=\"form-group\"><div class=\"col-md-12\"><strong>Zip / Postal Code:</strong></div><div class=\"col-md-12\"><input type=\"text\" name=\"zip_code\" class=\"form-control\" value=\"\"></div></div><div class=\"form-group\"><div class=\"col-md-12\"><strong>Phone Number:</strong></div><div class=\"col-md-12\"><input type=\"text\" name=\"phone_number\" class=\"form-control\" value=\"\"></div></div><div class=\"form-group\"><div class=\"col-md-12\"><strong>Email Address:</strong></div><div class=\"col-md-12\"><input type=\"text\" name=\"email_address\" class=\"form-control\" value=\"\"></div></div></div></div><!--SHIPPING METHOD END--><hr><!--CREDIT CART PAYMENT--><div class=\"panel panel-info\"><h4> Secure Payment </h4><br><div class=\"panel-body\"><div class=\"form-group\"><div class=\"col-md-12\"><strong>Card Type:</strong></div><div class=\"col-md-12\"><select id=\"CreditCardType\" name=\"CreditCardType\" class=\"form-control\"><option value=\"5\">Visa</option><option value=\"6\">MasterCard</option><option value=\"7\">American Express</option><option value=\"8\">Discover</option></select></div></div><div class=\"form-group\"><div class=\"col-md-12\"><strong>Credit Card Number:</strong></div><div class=\"col-md-12\"><input type=\"text\" class=\"form-control\" name=\"car_number\" value=\"\"></div></div><div class=\"form-group\"><div class=\"col-md-12\"><strong>Card CVV:</strong></div><div class=\"col-md-12\"><input type=\"text\" class=\"form-control\" name=\"car_code\" value=\"\"></div></div><div class=\"form-group\"><div class=\"col-md-12\"><strong>Expiration Date</strong></div><div class=\"col-lg-6 col-md-6 col-sm-6 col-xs-12\"><select class=\"form-control\" name=\"\"><option value=\"\">Month</option><option value=\"01\">01</option><option value=\"02\">02</option><option value=\"03\">03</option><option value=\"04\">04</option><option value=\"05\">05</option><option value=\"06\">06</option><option value=\"07\">07</option><option value=\"08\">08</option><option value=\"09\">09</option><option value=\"10\">10</option><option value=\"11\">11</option><option value=\"12\">12</option></select></div><div class=\"col-lg-6 col-md-6 col-sm-6 col-xs-12\"><select class=\"form-control\" name=\"\"><option value=\"\">Year</option><option value=\"2015\">2015</option><option value=\"2016\">2016</option><option value=\"2017\">2017</option><option value=\"2018\">2018</option><option value=\"2019\">2019</option><option value=\"2020\">2020</option><option value=\"2021\">2021</option><option value=\"2022\">2022</option><option value=\"2023\">2023</option><option value=\"2024\">2024</option><option value=\"2025\">2025</option></select></div></div><div class=\"form-group\"><div class=\"col-md-12\"><span>Pay secure using your credit card.</span></div><div class=\"col-md-12\"><ul class=\"cards\"><li class=\"visa hand\">Visa</li><li class=\"mastercard hand\">MasterCard</li><li class=\"amex hand\">Amex</li></ul><div class=\"clearfix\"></div></div></div><div class=\"form-group\"><div class=\"col-md-6 col-sm-6 col-xs-12\"><button type=\"submit\" class=\"btn btn-primary btn-submit-fix\">Place Order</button></div></div></div></div><!--CREDIT CART PAYMENT END--></div>", 1);
+var _hoisted_12 = {
   "class": "col-lg-4 col-md-4 col-sm-5 col-xs-12"
 };
-var _hoisted_12 = {
+var _hoisted_13 = {
   "class": "widget"
 };
-var _hoisted_13 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h4", {
+var _hoisted_14 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h4", {
   "class": "widget-title"
 }, "Order Summary", -1 /* HOISTED */);
-var _hoisted_14 = {
+var _hoisted_15 = {
   key: 0,
   "class": "summary-content"
 };
-var _hoisted_15 = {
+var _hoisted_16 = {
   "class": "summary-head"
 };
-var _hoisted_16 = {
+var _hoisted_17 = {
   "class": "summary-title"
 };
-var _hoisted_17 = {
+var _hoisted_18 = {
   "class": "summary-price"
 };
-var _hoisted_18 = {
+var _hoisted_19 = {
   "class": "summary-small-text pull-right"
 };
-var _hoisted_19 = {
+var _hoisted_20 = {
   "class": "text-danger"
 };
-var _hoisted_20 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+var _hoisted_21 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
   "class": "text-right"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("hr"), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("button", {
   "class": "btn btn-warning"
@@ -22760,12 +22761,12 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
       "class": "plan-selection",
       key: item.id
     }, [item.name ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_6, [_hoisted_7, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("label", _hoisted_8, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(item.name), 1 /* TEXT */), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_9, " Quantity: " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(item.quantity), 1 /* TEXT */), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", _hoisted_10, "Price/Unit: " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(item.sale_price) + "$", 1 /* TEXT */)])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true)]);
-  }), 128 /* KEYED_FRAGMENT */))])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_11, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_12, [_hoisted_13, ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($data.items, function (summaryItem) {
+  }), 128 /* KEYED_FRAGMENT */)), _hoisted_11])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_12, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_13, [_hoisted_14, ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($data.items, function (summaryItem) {
     return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", {
       "class": "summary-block",
       key: summaryItem.id
-    }, [summaryItem.name ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_14, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_15, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h5", _hoisted_16, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(summaryItem.name), 1 /* TEXT */)]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_17, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_18, "Total price: " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(summaryItem.total) + " $", 1 /* TEXT */)])])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true)]);
-  }), 128 /* KEYED_FRAGMENT */)), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_19, "Total cart price: " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.items.totalAmount) + " $", 1 /* TEXT */), _hoisted_20])])])])]);
+    }, [summaryItem.name ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_15, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_16, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h5", _hoisted_17, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(summaryItem.name), 1 /* TEXT */)]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_18, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_19, "Total price: " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(summaryItem.total) + " $", 1 /* TEXT */)])])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true)]);
+  }), 128 /* KEYED_FRAGMENT */)), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_20, "Total cart price: " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.items.totalAmount) + " $", 1 /* TEXT */), _hoisted_21])])])])]);
 }
 
 /***/ }),
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -68,7 +68,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <nav class="navbar navbar-expand-lg">
-                        <a class="navbar-brand" href="index.html">
+                        <a class="navbar-brand" href="/">
                             <img src="assets/images/logo.png" alt="Logo">
                         </a> <!-- Logo -->
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -80,7 +80,7 @@
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul id="nav" class="navbar-nav ml-auto">
                                 <li class="nav-item active">
-                                    <a data-scroll-nav="0" href="#home">Home</a>
+                                    <a data-scroll-nav="0" href="/">Home</a>
                                 </li>
                                 <li class="nav-item">
                                     <a data-scroll-nav="0" href="#product">Products</a>
@@ -102,6 +102,11 @@
                                 </li>
                                 @if (Route::has('login'))
                                     @auth
+                                        <li class="nav-item">
+                                            <a href="{{ route('logout') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log out</a>
+                                        </li>
+
+
                                         <Cart/>
                                     @else
                                         <li class="nav-item">
